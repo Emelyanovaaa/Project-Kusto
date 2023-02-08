badfcf265a7f95fda2822f0124ea10e71d8b4204
Emelyanova_Elena <Emelyanova0893@gmail.com>
2023-02-08 11:45:01 +0300

    added functionality

--- js/script.js
+++ js/script.js
@@ -9,6 +9,28 @@ const formElement = document.querySelector('.popup__profile-forms');
 const inputName = document.querySelector('.popup__form-name');
 const inputJob = document.querySelector('.popup__form-description');
 
+const inputSubmit = document.querySelector('.popup__form-btn');
+const nameProfile = document.querySelector('.profile__name-text');
+const jobProfile = document.querySelector('.profile__discription-text');
+
+const cardsContent = document.querySelector('.cards__content');
+
+const addButton = document.querySelector('.profile__add-btn');
+const addCard = document.querySelector('.popup__addCard');
+const addCardClose = document.querySelector('.addCard__cancel-btn');
+
+const addCardSubmit = document.querySelector('.popup__addForm-btn');
+const addCardName = document.querySelector('.popup__addForm-name');
+const addCardLink = document.querySelector('.popup__addForm-link');
+const cardName = document.querySelector('.cards__sub-text');
+const cardLink = document.querySelector('.cards__photo-img');
+
+const popupPhoto = document.querySelector('.popup__photo');
+const popupPhotoImg = document.querySelector('.popup__photo-img');
+const cancelPhoto = document.querySelector('.popup__cancel-photo');
+
+const popupPhotoSubText = document.querySelector('.popup__photo-subtext');
+
 function openPopup() {
     popup.classList.remove('is-hidden');
 };
@@ -23,16 +45,179 @@ editButton.addEventListener('click', openPopup);
 popupClosed.addEventListener('click', closePopup);
 
 
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function handleFormSubmit (event) {
+    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Получите значение полей jobInput и nameInput из свойства value
+    let inputNameValue = inputName.value;
+    let inputJobValue = inputJob.value;
 
-    console.log(inputName.value);
-    console.log(inputJob.value);
+    nameProfile.textContent = inputNameValue;
+    jobProfile.textContent = inputJobValue;
 
-    inputName.textContent = inputName.value;
-    inputJob.textContent = inputJob.value;
+    closePopup();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+inputSubmit.addEventListener('click', handleFormSubmit);
+
+    const initialCards = [
+        {
+            name: 'Alaska',
+            link: 'img/cards/Alaska.jpg'
+        },
+        {
+            name: 'Bogota',
+            link: 'img/cards/Bogota.jpg'
+        },
+        {
+            name: 'Egipt',
+            link: 'img/cards/Egipt.jpg'
+        },
+        {
+            name: 'France',
+            link: 'img/cards/France.jpg'
+        },
+        {
+            name: 'India',
+            link: 'img/cards/India.jpg'
+        },
+        {
+            name: 'London',
+            link: 'img/cards/London.jpg'
+        },
+    ];
+
+    for(let i = 0; i < initialCards.length; i++) {
+        const name = initialCards[i].name;
+        const link = initialCards[i].link;
+
+        createCard(name, link);
+    };
+
+    function createCard(name, link) {
+
+        const card = document.createElement('div');
+        card.classList.add('cards__card');
+
+        const cardPhoto = document.createElement('div');
+        cardPhoto.classList.add('cards__photo');
+
+        const cardSub = document.createElement('div');
+        cardSub.classList.add('cards__sub-photo');
+
+        const photo = document.createElement('img');
+        photo.classList.add('cards__photo-img');
+        photo.setAttribute('src', link);
+
+        const cancelCard = document.createElement('div');
+        cancelCard.classList.add('cards__cancel');
+
+        const cancelButton = document.createElement('button');
+        cancelButton.classList.add('cards__cancel-button');
+
+        const cancelIcon = document.createElement('img');
+        cancelIcon.classList.add('cards__cancel-icon');
+        cancelIcon.setAttribute('src', '/img/card_cancel_button.png');
+
+        const subName = document.createElement('div');
+        subName.classList.add('cards__sub-text');
+        subName.textContent = name;
+
+        const likeBtn = document.createElement('div');
+        likeBtn.classList.add('cards__like-btn');
+
+        const likeButton = document.createElement('button');
+        likeButton.classList.add('cards__btn-icon');
+
+        
+        cardPhoto.append(photo);
+        cancelCard.append(cancelButton);
+        cancelButton.append(cancelIcon);
+        likeBtn.append(likeButton);
+        /*likeButton.append(btnIcon);*/
+        cardSub.append(subName, likeBtn);
+        card.append(cardPhoto, cardSub, cancelCard);
+        cardsContent.append(card);
+    };
+
+    function open__addCard() {
+        addCard.classList.remove('is-hidden');
+    };
+    
+    function close__addCard() {
+        addCard.classList.add('is-hidden');
+        addCardName.value = addCardName.getAttribute('placeholder');
+        addCardLink.value = addCardLink.getAttribute('placeholder');
+    };
+    
+    addButton.addEventListener('click', open__addCard);
+    addCardClose.addEventListener('click', close__addCard);
+
+
+    function addCardFormSubmit (event) {
+        event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    
+       let cardName = addCardName.value;
+       let cardLink = addCardLink.value;
+
+        createCard(cardName, cardLink);
+        close__addCard();
+    };
+    
+    addCardSubmit.addEventListener('click', addCardFormSubmit);
+
+
+    cardsContent.addEventListener('click', function(event){
+        let likeTarget = event.target;
+
+        if(likeTarget.classList.contains('is-active')){
+            likeTarget.classList.remove('is-active');
+            return;
+        }
+
+        likeTarget.classList.add('is-active');
+    });
+
+
+    cardsContent.addEventListener('click', function(event){
+        const btn = event.target.closest('.cards__cancel-button');
+        if(!btn){
+            return;
+        }
+        btn.closest('.cards__card').remove();
+    });
+
+    cardsContent.addEventListener('click', function(event){
+        let img = event.target.closest('.cards__photo-img');
+        if(!img){
+            return;
+        }
+
+        let cardLinkValue = img.getAttribute('src');
+        popupPhotoImg.setAttribute('src', cardLinkValue);
+
+        let card = img.closest('.cards__card');
+        let cardSubName = card.querySelector('.cards__sub-text');
+        let nameImg = cardSubName.textContent;
+        popupPhotoSubText.textContent = nameImg;
+
+        let popupImg = document.querySelector('.popup__photo');
+        popupImg.classList.remove('is-hidden');
+    });
+
+
+
+   function closeImg(){
+        popupPhoto.classList.add('is-hidden');
+    };
+    
+    cancelPhoto.addEventListener('click', closeImg);
+
+
+
+
+
+
+
+
